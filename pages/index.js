d5daab219c232fe2790aa060b7accaa916b14657
gmvandanezi <guivandanezi@hotmail.com>
2022-11-11 19:46:28 -0300

    atualização da aula 2

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,26 @@
+import React from "react";
 import config from "../config.json";
 import styled from "styled-components";
 import { CSSReset } from "../src/components/CSSReset";
 import Menu from "../src/components/Menu";
 import { StyledTimeline } from "../src/components/Timeline";
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap');
+</style>
 
 function HomePage() {
-    const estilosDaHomePage = { 
-        //backgroundColor: "red" 
-    };
+
+    const [valorDoFiltro, setValorDoFiltro] = React.useState("amv");
 
     return (
         <>
-            <CSSReset/>
-            <div style={estilosDaHomePage}>
-                <Menu></Menu>
+
+            <CSSReset />
+            <div>
+                {/*Prop Drilling */}
+                <Menu valorDoFiltro={valorDoFiltro} setValorDoFiltro={setValorDoFiltro}></Menu>
                 <Header></Header>
-                <Timeline playlists={config.playlists}></Timeline>
+                <Timeline searchValue={valorDoFiltro} playlists={config.playlists}></Timeline>
             </div>
         </>
 
@@ -37,12 +42,21 @@ const StyledHeader = styled.div`
         width: 100%;
         padding: 16px 32px;
         gap: 16px;
-        margin-top: 50px;
     }
 `;
+
+const StyledBanner = styled.div`
+    background-image: url(${({bg}) => bg});
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position-y: -220px;
+    height: 280px;
+`;
+
 function Header() {
     return (
         <StyledHeader>
+            <StyledBanner bg={config.bg}/>
             <section className="user-info">
                 <img src={`https://github.com/${config.github}.png`}></img>
                 <div>
@@ -54,19 +68,23 @@ function Header() {
     )
 }
 
-function Timeline(props) {
+function Timeline({ searchValue, ...props }) {
     const playlistNames = Object.keys(props.playlists);
     return (
         <StyledTimeline>
             {playlistNames.map((playlistName) => {
                 const videos = props.playlists[playlistName];
                 return (
-                    <section>
+                    <section key={playlistName}>
                         <h2>{playlistName}</h2>
                         <div>
-                            {videos.map((video) => {
+                            {videos.filter((video) => {
+                                const titleNormalized = video.title.toLowerCase();
+                                const searchValueNormalized = searchValue.toLowerCase();
+                                return titleNormalized.includes(searchValueNormalized)
+                            }).map((video) => {
                                 return (
-                                    <a href={video.url}>
+                                    <a key={video.url} href={video.url}>
                                         <img src={video.thumb}></img>
                                         <span>
                                             {video.title}
--- src/components/CSSReset.js
+++ src/components/CSSReset.js
@@ -8,7 +8,7 @@ export const CSSReset = createGlobalStyle`
     box-sizing: border-box;
   }
   body {
-    font-family: sans-serif;
+    font-family: 'Poppins', sans-serif;;
   }
   /* NextJS */
   html {
